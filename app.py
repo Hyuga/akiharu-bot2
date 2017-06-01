f0c9f23548d2b4fec66dc315bdcab649317f3a2d
Hyuga <akiyu0901@gmail.com>
2017-06-01 22:48:46 +0900

    コミット
    コミット

--- app.py
+++ app.py
@@ -46,13 +46,13 @@ def makeWebhookResult(req):
         "displayText": speech,
         #"data": {},
         # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
+        "source": "agent"
     }
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    #print "Starting app on port %d" % port
 
     app.run(debug=True, port=port, host='0.0.0.0')
